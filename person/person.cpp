1b01b366ec8322eb6808ee09f684f7a0b2ee16b9
ahmad2806 <ahmadhashem2806@gmail.com>
2018-12-04 12:01:11 +0200

    init the api

---
+++ person/person.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ahmad on 04/12/2018.
+//
+
+#include "person.h"
---
+++ person/person.h
@@ -0,0 +1,14 @@
+//
+// Created by ahmad on 04/12/2018.
+//
+
+#ifndef PERSON_PERSON_H
+#define PERSON_PERSON_H
+
+
+class person {
+
+};
+
+
+#endif //PERSON_PERSON_H

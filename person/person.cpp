4ebdf26b15523addd941a7a672cd6125a4be49cf
ahmad2806 <ahmadhashem2806@gmail.com>
2018-12-06 10:08:40 +0200

    template allocator

--- person/person.cpp
+++ person/person.cpp
@@ -1,41 +1,21 @@
-//
-// Created by ahmad on 04/12/2018.
-//
-
-#include "person.h"
 #include <iostream>
-#include <stdlib.h>
+#include <stddef.h>
 #include "cstring"
+#include "person.h"
 
-unsigned char Person::s_pool[sizeof(Person) * Person::POOL_SIZE];
-void *Person::s_free_list = Person::init_memory_pool();
 
-void *Person::init_memory_pool() {
-    unsigned char *memory_pool = Person::s_pool;
-
-    for (int i = 0; i < Person::POOL_SIZE - 1; i++) {
-
-        *(void **) memory_pool = memory_pool + sizeof(Person);
-        memory_pool = memory_pool + sizeof(Person);
-    }
-    *(void **) memory_pool = NULL;
-    return Person::s_pool;
+Person::Person(char name[32], unsigned int id, unsigned int age):m_id(id),m_age(age){
+    strcpy(m_full_name,name);
 }
 
-void *Person::operator new(std::size_t) {
-    void *free_item = Person::s_free_list;
-    if (!free_item) {
-        throw std::bad_alloc();
-    }
-    Person::s_free_list = *(void **) s_free_list;
-    return free_item;
+Person::Person(Person &other) {
+    strcpy(m_full_name,other.fullname);
+    m_id = other.id;
+    m_age = other.m_age;
 }
 
-void Person::operator delete(void *ptr) {
-    *(void **) ptr = Person::s_free_list;
-    Person::s_free_list = ptr;
+char *Person::get_full_name() {
+    return full_name;
 }
 
-Person::Person(char name[32], unsigned int id, unsigned int age) : id(id), m_age(age) {
-    strcpy(fullname, name);
-}
+
--- person/person.h
+++ person/person.h
@@ -4,32 +4,17 @@
 
 #ifndef PERSON_PERSON_H
 #define PERSON_PERSON_H
-
-
-#include <cstddef>
-
-
 class Person {
-    static void *s_free_list;
-    static unsigned char s_pool[];
-    const static int POOL_SIZE = 5;
+
 public:
-    void *operator new(std::size_t count);
+    Person(Person &other);
+    Person(char full_name[32], unsigned int id,unsigned int age);
+    char* get_full_name();
 
-    void operator delete(void *ptr);
-
-    Person(char fullname[32], unsigned int id, unsigned int age);
-
-    static void *init_memory_pool();
-
-    char fullname[32];
-    unsigned int id;
-    unsigned char m_age;
 private:
-    void *operator new[](size_t);
-
-    void operator delete[](void *);
-
+    char m_full_name[32];
+    unsigned int m_id;
+    unsigned char m_age;
 
 };
 
